c3d93780e393db16c5fa2c9e4577d7029e35f414
SSE World <114757626+sseworld@users.noreply.github.com>
2024-06-07 10:52:25 +0000

    Components are completed

---
+++ components/newuserpage/Progresstitle.tsx
@@ -0,0 +1,12 @@
+function Progresstitle() {
+  return (
+    <h2
+      id="signupTitle"
+      className="font-medium text-lg animate-[fadeInUp] duration-300 block"
+    >
+      Welcome, select an account type.
+    </h2>
+  );
+}
+
+export default Progresstitle;
